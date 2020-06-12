9be48ebb27554ad90a38818af6ef9a24eaff5892
Bella <61517788+nabella216@users.noreply.github.com>
2020-06-12 16:14:34 +0800

    Update linear.py
    the indentations of 'for' loop

--- linear.py
+++ linear.py
@@ -1,24 +1,24 @@
-#created by Norhasliza Yusof for SIF2007 Numerical and Computational course
-#Gaussian Elimination Code Example
+
 
 import numpy as np
 
 #Input your matrix A using numpy arrays
-A = np.array([[4.0,2.0,-1.0],[1.0,4.0,1.0],[2.0,-1.0,4.0]])
+#A = np.array([[4.0,2.0,0.0],[2.0,3.0,1.0],[0.0,1.0,2.5]])
+A = np.array([[4.0,1.0],[0.0,5.0]])
 # Define matrix B using numpy arrays
-b = np.array([5.0,12.0,12.0])
+b = np.array([2.0,10.0])
 
 def gauss(A,b):
     n =  len(b) #define lenght/size of matrix
-  for j in range(0,n-1):
+    for j in range(0,n-1):
       for i in range(j+1,n):
         lam = A[i,j]/A[j,j]
         A[i] = A[i] - lam*A[j]
         b[i] = b[i] - lam*b[j]
-  x = np.zeros(n)
-  for j in range (n-1,-1,-1):
+        x = np.zeros(n)
+    for j in range (n-1,-1,-1):
      x[j]=b[j]/A[j,j]
      b = b-x[j]*A[:,j]
-  return x
+    return x
 
 print(gauss(A,b))
